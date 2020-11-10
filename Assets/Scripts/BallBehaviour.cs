b7952887cf34b0f67916d18360a3c47af4dacdca
krissuNew <krzysztofrusak91@gmail.com>
2020-11-10 22:32:37 +0100

    Added first behaviour to the ball

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BallBehaviour : MonoBehaviour
 {
+
+    float movementSpeed;
+    private Rigidbody2D ballBody;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ballBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // dodamy nowe funkcje
+        
+        
+       
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        movementSpeed = Random.Range(-3f, 3f);
+        ballBody.velocity = new Vector2(movementSpeed * 2f, movementSpeed - 1f);
     }
 }
